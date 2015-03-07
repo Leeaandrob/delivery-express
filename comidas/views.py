43beb0237548491d95aa40a434581149b25021da
Leandro Barbosa <leandrobar93@gmail.com>
2015-03-07 17:28:14 -0300

    Adding the FoodCreate

--- comidas/views.py
+++ comidas/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.core.urlresolvers import reverse, reverse_lazy
 
 from .models import Food
 
 class FoodList(ListView):
     model = Food
     template_name = "comidas/foodlist.html"
+
+
+class FoodCreate(CreateView):
+    model = Food
+    template_name = "comidas/createfood.html"
+    success_url = reverse_lazy('foodlist')
+
