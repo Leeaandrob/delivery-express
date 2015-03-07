7c0e3aba8ea7d1583e2e254be40e65e0ca09103a
Leandro Barbosa <leandrobar93@gmail.com>
2015-03-07 17:28:48 -0300

    Return the old urls

--- delivery/urls.py
+++ delivery/urls.py
@@ -1,3 +1,4 @@
+#coding: utf-8
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic.base import TemplateView
@@ -5,11 +6,6 @@ from django.views.generic.base import TemplateView
 from comidas.views import FoodList
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'delivery.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'^$', TemplateView.as_view(template_name='delivery/base.html'), name = 'base'),
-    url(r'^$', FoodList.as_view(), name='foodlist'),
+    url(r'^$', TemplateView.as_view(template_name='delivery/base.html'), name = 'base'),
 )
