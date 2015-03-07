c32d8e5ae2fc1ac3a4ef0287f909e4d58a5553f0
Leandro Barbosa <leandrobar93@gmail.com>
2015-03-07 17:59:06 -0300

    Adding the route updatefood

--- comidas/urls.py
+++ comidas/urls.py
@@ -1,9 +1,10 @@
 #coding: utf-8
 from django.conf.urls import patterns, include, url
 
-from .views import FoodList, FoodCreate
+from .views import FoodList, FoodCreate, FoodUpdate 
 
 urlpatterns = patterns('',
     url(r'^$', FoodList.as_view(), name='foodlist'),
     url(r'^createfood/$', FoodCreate.as_view(), name='createfood'),
+    url(r'^updatefood/$', FoodUpdate.as_view(), name='updatefood'),
 )
