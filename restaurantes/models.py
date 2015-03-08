1d9f4af4ec580e4b6b34ef6e94dd25ed9fe0ad67
Leandro Barbosa <leandrobar93@gmail.com>
2015-03-08 16:27:57 -0300

    Adding the __unicode__

--- restaurantes/models.py
+++ restaurantes/models.py
@@ -3,5 +3,9 @@ from pessoas.models import Person
 from comidas.models import Food
 
 class Restaurant(Person):
-	cnpj = models.CharField(max_length=14,blank=True,null=True)
-	foods = models.ManyToManyField(Food)
+    cnpj = models.CharField(max_length=14,blank=True,null=True)
+    foods = models.ManyToManyField(Food)
+
+    def __unicode__(self):
+        return self.cnpj
+
